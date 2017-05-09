a9cd5a6674358f17dcb245aa4627f077f8a2c6d3
Mykolas Sindeikis <mykolas.sindeikis@gmail.com>
2017-05-09 22:52:29 +0300

    Update utils.test.js

--- src/components/utils.test.js
+++ src/components/utils.test.js
@@ -1,6 +1,6 @@
 import { unflatten } from "./utils.js";
 
-test("addTodo should add the passed todo to the list", () => {
+test("unflatten should add childrens property to objects and filled them accordingly", () => {
   const initialTreeList = [
     { id: 1, name: "Lietuva", parentid: 0 },
     { id: 2, name: "Vilnius", parentid: 1 },
